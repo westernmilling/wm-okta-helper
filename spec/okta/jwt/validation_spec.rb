18923b6898137126c568719ad8552bb53af298c9
Jose C Fernandez <jfernandez@westernmilling.com>
2018-07-11 16:50:55 -0400

    Add coverage.

--- spec/okta/jwt/validation_spec.rb
+++ spec/okta/jwt/validation_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Okta::Jwt::Validation do
   it 'has a version number' do
     expect(Okta::Jwt::Validation::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(true).to eq(true)
-  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,9 @@
 
 require 'bundler/setup'
 require 'okta/jwt/validation'
+require 'simplecov'
+
+SimpleCov.start
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
