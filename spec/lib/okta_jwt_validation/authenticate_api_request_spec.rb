65357af7fd59541fc4ea4d089a24848b60fc05bb
Jose C Fernandez <jfernandez@westernmilling.com>
2018-07-16 15:29:08 -0400

    Add test cases.

--- spec/lib/okta_jwt_validation/authenticate_api_request_spec.rb
+++ spec/lib/okta_jwt_validation/authenticate_api_request_spec.rb
@@ -4,26 +4,67 @@ require 'support/test_request'
 
 RSpec.describe OktaJwtValidation::AuthenticateApiRequest do
   describe '#call' do
-    let(:expected_request) do
-      TestRequest.new
-    end
-    let(:okta_client_id) { 'yEqoXVxrwxFImVKHdtJF' }
+    context 'with correct credentials' do
+      let(:expected_request) do
+        TestRequest.new
+      end
+      let(:okta_client_id) { 'yEqoXVxrwxFImVKHdtJF' }
 
-    let(:subject) do
-      OktaJwtValidation::AuthenticateApiRequest.new(
-        request: expected_request,
-        okta_org: 'westernmilling',
-        okta_domain: 'okta',
-        okta_client_id: okta_client_id
-      ).call
+      let(:subject) do
+        OktaJwtValidation::AuthenticateApiRequest.new(
+          request: expected_request,
+          okta_org: 'westernmilling',
+          okta_domain: 'okta',
+          okta_client_id: okta_client_id
+        ).call
+      end
+
+      it 'returns expected message hash' do
+        expect(subject).to have_key('iss')
+        expect(subject).to have_key('aud')
+        expect(subject['aud']).to eq okta_client_id
+        expect(subject['iss']).to eq 'https://westernmilling.okta.com'
+        expect(Time.strptime(subject['exp'].to_s, '%s') < Time.now.utc)
+      end
     end
 
-    it 'returns expected message hash' do
-      expect(subject).to have_key('iss')
-      expect(subject).to have_key('aud')
-      expect(subject['aud']).to eq okta_client_id
-      expect(subject['iss']).to eq 'https://westernmilling.okta.com'
-      expect(Time.strptime(subject['exp'].to_s, '%s') < Time.now.utc)
+    context 'with an incorrect client id' do
+      let(:expected_request) do
+        TestRequest.new
+      end
+      let(:okta_client_id) { 'wrong_key_here' }
+
+      let(:subject) do
+        OktaJwtValidation::AuthenticateApiRequest.new(
+          request: expected_request,
+          okta_org: 'westernmilling',
+          okta_domain: 'okta',
+          okta_client_id: okta_client_id
+        ).call
+      end
+
+      it 'returns nil' do
+        expect(subject).to be_nil
+      end
+    end
+
+    context 'with missing credentials' do
+      let(:expected_request) do
+        TestRequest.new
+      end
+
+      let(:subject) do
+        OktaJwtValidation::AuthenticateApiRequest.new(
+          request: expected_request,
+          okta_org: '',
+          okta_domain: '',
+          okta_client_id: ''
+        ).call
+      end
+
+      it 'returns nil' do
+        expect(subject).to be_nil
+      end
     end
   end
 end
