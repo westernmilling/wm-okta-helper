0077f725d37596b51cc9eb5f32c88e72e0338cb1
Jose C Fernandez <jfernandez@westernmilling.com>
2018-07-26 11:06:47 -0400

    Discriminate between http requests vs https.
    Wm-okta-helper needs to be able to discriminate between http requests vs https.

--- lib/wm_okta_helper/post_request.rb
+++ lib/wm_okta_helper/post_request.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'addressable/uri'
 require 'net/http'
 require 'uri'
 
@@ -8,29 +9,63 @@ module WmOktaHelper
     def initialize(options)
       @request_body = options[:request_body]
       @url = options[:url]
+      @schemes = %w[http https].freeze
     end
 
     def call
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      return invalid_url unless valid_url?(url)
+      post_request
+    end
 
+    protected
+
+    def post_request
+      http = Net::HTTP.new(uri.host, uri.port)
+      open_ssl(http) if parsed.scheme == 'https'
+
+      response = http.request(request_configuration)
+      JSON.parse(response.read_body)
+    end
+
+    def uri
+      URI(url)
+    end
+
+    def request_configuration
       request = Net::HTTP::Post.new(uri)
       request['Accept'] = 'application/json'
       request['Content-Type'] = 'application/json'
       request['Cache-Control'] = 'no-cache'
       request.body = request_body.to_json
-
-      response = http.request(request)
-      JSON.parse(response.read_body)
+      request
     end
 
-    attr_accessor :request_body, :url
-
-    private
-
-    def uri
-      URI(url)
+    def parsed
+      Addressable::URI.parse(uri) or return false
     end
+
+    def valid_url?(parsed)
+      schemes.include?(
+        parsed.scan(URI::DEFAULT_PARSER.make_regexp)[0][0]
+      )
+    rescue Addressable::URI::InvalidURIError
+      false
+    end
+
+    def invalid_url
+      {
+        "errors": [
+          "The url #{url} is not valid"
+        ]
+      }
+    end
+
+    def open_ssl(http)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      http
+    end
+
+    attr_accessor :request_body, :url, :schemes
   end
 end
