ea82f61ef3ee0659224117e5eac51c048e70b854
Samantha Torres <37547118+storres-westernmilling@users.noreply.github.com>
2018-10-02 11:57:22 -0700

    Throw exceptions if nil or not authorized (#15)
    * Throw exceptions if nil or not authorized
    
    For https://github.com/westernmilling/otis/pull/355

--- spec/lib/wm_okta_helper/create_session_spec.rb
+++ spec/lib/wm_okta_helper/create_session_spec.rb
@@ -2,144 +2,52 @@
 
 RSpec.describe WmOktaHelper::CreateSession do
   describe '#call' do
-    let(:request_headers) do
-      {
-        'Accept' => 'application/json',
-        'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-        'Cache-Control' => 'no-cache',
-        'Content-Type' => 'application/json',
-        'Host' => 'westernmilling.okta.com',
-        'User-Agent' => 'Ruby'
-      }
+    let(:subject) do
+      WmOktaHelper::CreateSession.new(
+        username: username,
+        password: 'pwd',
+        okta_org: 'westernmilling',
+        okta_domain: 'okta'
+      ).call
+    end
+
+    let(:response) do
+      f = File.join(%W[./ spec support fixtures #{fixture_file}])
+      JSON.parse(File.read(f))
+    end
+
+    before do
+      allow_any_instance_of(WmOktaHelper::PostRequest).to receive(:call)
+        .and_return(response)
     end
 
     context 'with correct credentials' do
-      def mock_user
-        user = double('user', id: 1,
-                              username: 'Test User',
-                              password: 'long_convoluted_password')
-
-        user
-      end
-
-      before do
-        mock_user
-      end
-
-      let(:username) do
-        mock_user.username
-      end
-
-      let(:password) do
-        mock_user.password
-      end
-
-      let(:subject) do
-        WmOktaHelper::CreateSession.new(
-          username: username,
-          password: password,
-          okta_org: 'westernmilling',
-          okta_domain: 'okta'
-        ).call
-      end
-
-      let(:request_body) do
-        { username: 'Test User',
-          password: 'long_convoluted_password',
-          options: {
-            multiOptionalFactorEnroll: true,
-            warnBeforePasswordExpired: true
-          } }.to_json
-      end
-
-      let(:full_request) do
-        { body: request_body,
-          headers: request_headers }
-      end
-
+      let(:username) { 'Test User' }
       let(:fixture_file) { 'create_session_success.json' }
 
-      let(:response_body) do
-        File.read(File.join(%W[./ spec support fixtures #{fixture_file}]))
-      end
-
       it 'returns expected message hash' do
-        stub_request(:post, 'https://westernmilling.okta.com/api/v1/authn')
-          .with(full_request)
-          .to_return(status: 200, body: response_body, headers: {})
-
-        expect(
-          JSON.parse(subject)
-        ).to have_key('sessionToken')
-        expect(
-          JSON.parse(subject)
-        ).to have_key('status')
+        expect(subject).to have_key('sessionToken')
       end
     end
 
     context 'with incorrect credentials' do
-      def mock_user
-        user = double('user', id: 1,
-                              username: 'Test User',
-                              password: 'oh_bad_password_are_thee')
-
-        user
-      end
-
-      before do
-        mock_user
-      end
-
-      let(:username) do
-        mock_user.username
-      end
-
-      let(:password) do
-        mock_user.password
-      end
-
-      let(:subject) do
-        WmOktaHelper::CreateSession.new(
-          username: username,
-          password: password,
-          okta_org: 'westernmilling',
-          okta_domain: 'okta'
-        ).call
-      end
-
-      let(:request_body) do
-        { username: 'Test User',
-          password: 'oh_bad_password_are_thee',
-          options: {
-            multiOptionalFactorEnroll: true,
-            warnBeforePasswordExpired: true
-          } }.to_json
-      end
-
-      let(:full_request) do
-        {
-          body: request_body,
-          headers: request_headers
-        }
-      end
-
+      let(:username) { 'Test User' }
       let(:fixture_file) { 'create_session_failure.json' }
 
-      let(:response_body) do
-        File.read(File.join(%W[./ spec support fixtures #{fixture_file}]))
+      it 'returns expected error hash' do
+        expect { subject }.to raise_error(RuntimeError, 'Not authorized')
       end
+    end
+
+    context 'with incorrect credentials' do
+      let(:username) { nil }
+      let(:fixture_file) { 'create_session_failure.json' }
 
       it 'returns expected error hash' do
-        stub_request(:post, 'https://westernmilling.okta.com/api/v1/authn')
-          .with(full_request)
-          .to_return(status: 401, body: response_body, headers: {})
-
-        expect(
-          JSON.parse(subject.to_json)
-        ).to have_key('errorCode')
-        expect(
-          JSON.parse(subject.to_json)
-        ).to have_key('errorSummary')
+        expect { subject }.to raise_error(
+          RuntimeError,
+          'Missing configuration variable: [:username]'
+        )
       end
     end
   end
