b0f5099065886d3a3d8593efb897941fb88398b8
Jose C Fernandez <jfernandez@westernmilling.com>
2020-01-20 09:40:13 -0500

    Fix

--- lib/wm_okta_helper/authenticate_api_request.rb
+++ lib/wm_okta_helper/authenticate_api_request.rb
@@ -76,7 +76,7 @@ module WmOktaHelper
       @token = parse_token
       if @token['iss'] != site ||
          @token['aud'] != client_id ||
-         (@token['exp'].to_i < Time.now.utc.to_i && !Rails.env == 'react_test')
+         (@token['exp'].to_i < Time.now.utc.to_i && Rails.env != 'react_test')
         return false
       else
         return true
