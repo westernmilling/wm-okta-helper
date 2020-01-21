bf1825f61acbe5a43edad31e037def763ce34404
Jose C Fernandez <jfernandez@westernmilling.com>
2020-01-21 17:58:24 -0500

    Fix test.

--- spec/lib/wm_okta_helper/authenticate_api_request_spec.rb
+++ spec/lib/wm_okta_helper/authenticate_api_request_spec.rb
@@ -4,22 +4,27 @@ require 'support/test_request'
 
 RSpec.describe WmOktaHelper::AuthenticateApiRequest do
   describe '#call' do
+    let(:okta_client_id) { 'yEqoXVxrwxFImVKHdtJF' }
+    let!(:expected_request) { TestRequest.new }
+
+    let!(:validations_options) do
+      {
+        request: expected_request,
+        okta_org: 'westernmilling',
+        okta_domain: 'okta',
+        okta_client_id: okta_client_id
+      }
+    end
+
+    let!(:no_validations_options) do
+      validations_options.merge(ignore_validations: true)
+    end
+
+    let!(:subject) do
+      WmOktaHelper::AuthenticateApiRequest.new(validations_options).call
+    end
+
     context 'with correct credentials' do
-      let(:expected_request) do
-        TestRequest.new
-      end
-
-      let(:okta_client_id) { 'yEqoXVxrwxFImVKHdtJF' }
-
-      let(:subject) do
-        WmOktaHelper::AuthenticateApiRequest.new(
-          request: expected_request,
-          okta_org: 'westernmilling',
-          okta_domain: 'okta',
-          okta_client_id: okta_client_id
-        ).call
-      end
-
       it 'returns expected message hash' do
         Timecop.freeze do
           result = subject
@@ -33,19 +38,7 @@ RSpec.describe WmOktaHelper::AuthenticateApiRequest do
     end
 
     context 'with an incorrect client id' do
-      let(:expected_request) do
-        TestRequest.new
-      end
-      let(:okta_client_id) { 'wrong_key_here' }
-
-      let(:subject) do
-        WmOktaHelper::AuthenticateApiRequest.new(
-          request: expected_request,
-          okta_org: 'westernmilling',
-          okta_domain: 'okta',
-          okta_client_id: okta_client_id
-        ).call
-      end
+      let!(:okta_client_id) { 'wrong_key_here' }
 
       it 'returns nil' do
         expect(subject).to be_nil
@@ -53,16 +46,9 @@ RSpec.describe WmOktaHelper::AuthenticateApiRequest do
     end
 
     context 'with missing credentials' do
-      let(:expected_request) do
-        TestRequest.new
-      end
-
       let(:subject) do
         WmOktaHelper::AuthenticateApiRequest.new(
-          request: expected_request,
-          okta_org: '',
-          okta_domain: '',
-          okta_client_id: ''
+          validations_options.merge(okta_client_id: '')
         ).call
       end
 
@@ -72,33 +58,21 @@ RSpec.describe WmOktaHelper::AuthenticateApiRequest do
     end
 
     context 'with expired token' do
-      let(:expected_request) do
-        TestRequest.new
-      end
+      context 'without validations' do
+        let!(:subject) do
+          WmOktaHelper::AuthenticateApiRequest.new(no_validations_options).call
+        end
 
-      let(:okta_client_id) { 'yEqoXVxrwxFImVKHdtJF' }
-
-      let(:subject) do
-        WmOktaHelper::AuthenticateApiRequest.new(
-          request: expected_request,
-          okta_org: 'westernmilling',
-          okta_domain: 'okta',
-          okta_client_id: okta_client_id
-        ).call
-      end
-
-      before do
-        Timecop.freeze
-      end
-
-      after do
-        Timecop.return
-      end
-
-      it 'returns nil' do
-        expected_request # Trigger this before time travel
-        Timecop.travel(Time.now.utc + 2.months)
-        expect(subject).to be_nil
+        it 'returns nil' do
+          Timecop.freeze do
+            result = subject
+            expect(result).to have_key('iss')
+            expect(result).to have_key('aud')
+            expect(result['aud']).to eq okta_client_id
+            expect(result['iss']).to eq 'https://westernmilling.okta.com'
+            expect(subject['exp'].to_i).to eq((Time.now.utc + 1.month).to_i)
+          end
+        end
       end
     end
   end
